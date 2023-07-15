fe1598865e3f027ca56b8f1a25ae0d94c0010370
Daniel Silva <daniel.silva.prg@gmail.com>
2023-07-15 11:28:12 +0100

    dope modal working

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,6 @@
 const info = document.getElementById("info")
 const map = document.getElementById("map")
 
-/**
- * One click serves available residences
- */
 map.addEventListener("click", (event) => {
     info.innerHTML = avaResidences
 })
