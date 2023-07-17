227018d69bf5e00a56327e039e2eaa8ac3295a89
Daniel Silva <daniel.silva.prg@gmail.com>
2023-07-17 12:50:15 +0100

    forms for claimResidence and newReview

---
+++ assets/js/claimResidence.js
@@ -0,0 +1,108 @@
+//const claimExistingResidence = ???
+const claimResidence = 
+`
+<form>
+                    <div class="space-y-12">
+                        <div class="border-b border-gray-900/10 pb-12">
+                            <h2 class="text-base font-semibold leading-7 text-gray-900">Claim Residence</h2>
+
+                            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div class="sm:col-span-2 sm:col-start-1">
+                                    <label for="city" class="block text-sm font-medium leading-6 text-gray-900">City</label>
+                                    <div class="mt-2">
+                                      <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                  </div>
+                          
+                                  <div class="sm:col-span-2">
+                                    <label for="region" class="block text-sm font-medium leading-6 text-gray-900">Street</label>
+                                    <div class="mt-2">
+                                      <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                  </div>
+                          
+                                  <div class="sm:col-span-2">
+                                    <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">Residence Number</label>
+                                    <div class="mt-2">
+                                      <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                  </div>
+
+                              <div class="sm:col-span-3">
+                                <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Floor</label>
+                                <div class="mt-2">
+                                  <input type="text" name="first-name" id="first-name" placeholder=" (Optional)" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                </div>
+                              </div>
+                      
+                              <div class="sm:col-span-3">
+                                <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Direction</label>
+                                <div class="mt-2">
+                                  <input type="text" name="last-name" id="last-name" placeholder=" (Optional) " autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                </div>
+                              </div>
+                      
+                              <div class="sm:col-span-full">
+                                <fieldset>
+                                    <legend class="text-sm font-semibold leading-6 text-gray-900">Is the property available for renting?</legend>
+                                    <div class="mt-6 space-y-6">
+                                      <div class="flex items-center gap-x-3">
+                                        <input id="push-everything" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                        <label for="push-everything" class="block text-sm font-medium leading-6 text-gray-900">Yes</label>
+                                      </div>
+                                      <div class="flex items-center gap-x-3">
+                                        <input id="push-email" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                        <label for="push-email" class="block text-sm font-medium leading-6 text-gray-900">No</label>
+                                      </div>
+                                    </div>
+                                  </fieldset>
+                              </div>
+
+                              <div class="col-span-full">
+                                <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Proof of Ownership</label>
+                                <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                                  <div class="text-center">
+                                    <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                      <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
+                                    </svg>
+                                    <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                                      <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                        <span>Upload a file</span>
+                                        <input id="proofResidence" name="proofResidence" type="file">
+                                      </label>
+                                      <p class="pl-1">or drag and drop</p>
+                                    </div>
+                                    <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <div class="col-span-full">
+                                <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Property Images (optional)</label>
+                                <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                                  <div class="text-center">
+                                    <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                      <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
+                                    </svg>
+                                    <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                                      <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                        <span>Upload a file</span>
+                                        <input id="file-upload" name="file-upload" type="file" class="sr-only" multiple>
+                                      </label>
+                                      <p class="pl-1">or drag and drop</p>
+                                    </div>
+                                    <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                                  </div>
+                                </div>
+                              </div>
+
+                            </div>
+                          </div>
+                    </div>
+                  
+                    <div class="mt-6 flex items-center justify-end gap-x-6">
+                      <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Claim</button>
+                    </div>
+                  </form>
+
+`
--- assets/js/index.js
+++ assets/js/index.js
@@ -7,11 +7,13 @@ const info = document.getElementById("info")
 const map = document.getElementById("map")
 
 map.addEventListener("click", (event) => {
-    info.innerHTML = avaResidences
+    //info.innerHTML = avaResidences
+    info.innerHTML = newReview
 })
 
 map.addEventListener("dblclick", (event) => {
-    info.innerHTML = reviewsSearch + reviews
+    //info.innerHTML = reviewsSearch + reviews
+    info.innerHTML = claimResidence
 })
 
 function showResidenceDetails(){
