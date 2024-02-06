2076411832914a9210daf938bf3edd415d811cbf
Daniel Silva <daniel.silva.prg@gmail.com>
2024-02-06 10:34:50 +0000

    fix condition on filters

--- src/pages/profile/filters.js
+++ src/pages/profile/filters.js
@@ -5,55 +5,52 @@ const byRentPriceMin = document.getElementById("byRentPriceMin")
 const byRentPriceMax = document.getElementById("byRentPriceMax")
 const enable = document.getElementById("enable")
 
-async function getFilters(){
+async function getFilters() {
 
-    try{
-    const response = await (await fetch(`${apis.notification}getFilters`, {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json",
-            "authorization": `baer ${token}`
-        }
-    })).json()
+    try {
+        const response = await (await fetch(`${apis.notification}getFilters`, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+                "authorization": `baer ${token}`
+            }
+        })).json()
 
-    console.log(filters)   
-    byCities.value = response.filters.byCities
-    byRentPriceMin.value = response.filters.byRentPriceMin
-    byRentPriceMax.value = response.filters.byRentPriceMax
-    
-    }catch(e){
+        console.log(filters)
+        byCities.value = response.filters.byCities
+        byRentPriceMin.value = response.filters.byRentPriceMin
+        byRentPriceMax.value = response.filters.byRentPriceMax
+
+    } catch (e) {
         console.log(e)
     }
 }
 
-async function setFilters(){  
+async function setFilters() {
     console.log(enable.checked)
-    console.log(token)
 
-    if(enable.checked){
-        const data = {
-            byCities: byCities.value, 
-            byRentPriceMin: byRentPriceMin.value, 
-            byRentPriceMax: byRentPriceMax.value, 
-            enable: enable.checked
-        }
+    const data = {
+        byCities: byCities.value,
+        byRentPriceMin: byRentPriceMin.value,
+        byRentPriceMax: byRentPriceMax.value,
+        enable: enable.checked
+    }
 
-        fetch(`${apis.notification}setFilters`, {
-            "method": "POST",
-            headers: {
-                "Content-type": "application/json",
-                "authorization": `baer ${token}`
-            },
-            body: JSON.stringify(data)
-        })
+    fetch(`${apis.notification}setFilters`, {
+        "method": "POST",
+        headers: {
+            "Content-type": "application/json",
+            "authorization": `baer ${token}`
+        },
+        body: JSON.stringify(data)
+    })
         .then(async response => {
 
-            if(!response.ok) throw new Error((await response.json()).msg.message);
+            if (!response.ok) throw new Error((await response.json()).msg.message);
 
             console.log(response)
         })
         .catch(e => console.log(e))
-    }
 }
 
 getFilters()
