853ae48442d914eab68ceb55bfb6844c28af0ca3
Daniel Silva <daniel.silva.prg@gmail.com>
2024-02-07 17:51:10 +0000

    show review status (pending, approved)

--- src/pages/profile/madeReviews.js
+++ src/pages/profile/madeReviews.js
@@ -10,19 +10,19 @@ const totalReviews = document.getElementById("totalReviews")
 const modalReview = document.getElementById("modal-review")
 const modalRating = document.getElementById("modal-rating")
 const modalCreatedOn = document.getElementById("modal-createdOn")
-const modalImgs= document.getElementById("modal-imgs")
+const modalImgs = document.getElementById("modal-imgs")
 
-function toggleModalReviews(modalID, reviewData = undefined){
+function toggleModalReviews(modalID, reviewData = undefined) {
 
-    if(reviewData){
-        modalReview.innerHTML = reviewData.rev.review
-        modalRating.innerHTML = reviewData.rev.rating
-        modalCreatedOn.innerHTML = reviewData.rev.createdOn
-        modalImgs.innerHTML = ""
+  if (reviewData) {
+    modalReview.innerHTML = reviewData.rev.review
+    modalRating.innerHTML = reviewData.rev.rating
+    modalCreatedOn.innerHTML = reviewData.rev.createdOn
+    modalImgs.innerHTML = ""
 
-        for(let x = 0; x < reviewData.rev.imgs; x++){
-            modalImgs.innerHTML += /*html */
-            `<div class="flex w-1/3 flex-wrap">
+    for (let x = 0; x < reviewData.rev.imgs; x++) {
+      modalImgs.innerHTML += /*html */
+        `<div class="flex w-1/3 flex-wrap">
             <div class="w-full p-1 md:p-2">
               <img
                 alt="gallery"
@@ -30,28 +30,28 @@ function toggleModalReviews(modalID, reviewData = undefined){
                 src="../../../assets/images/reviewImgs/review-${reviewData.rev.id}/rImg-${x}.gif" />
             </div>
           </div>`
-        }
     }
+  }
 
-    document.getElementById(modalID).classList.toggle("hidden");
-    document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
-    document.getElementById(modalID).classList.toggle("flex");
-    document.getElementById(modalID + "-backdrop").classList.toggle("flex");
-        
+  document.getElementById(modalID).classList.toggle("hidden");
+  document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+  document.getElementById(modalID).classList.toggle("flex");
+  document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+
 }
 
 aggrData()
-.then(response => {
-  
+  .then(response => {
+
     console.log("response")
     console.log(response)
 
     const ownedRevs = []
     Object.values(response.revs).forEach(element => {
-        element.map(row => {
-            console.log(userInfo.userId)
-            if(row.rev.userId == userInfo.userId) ownedRevs.push(row); 
-        })
+      element.map(row => {
+        console.log(userInfo.userId)
+        if (row.rev.userId == userInfo.userId) ownedRevs.push(row);
+      })
     })
 
     totalReviews.innerHTML = ownedRevs.length
@@ -60,19 +60,21 @@ aggrData()
     console.log(ownedRevs)
 
     ownedRevs.map(
-        // {rev, location}
-        r => {
+      // {rev, location}
+      r => {
+        const status = (r.rev.approved)? "<span style='color:green'>approved</span>" : "<span style='color:orange'>pending</span>"
 
-       const row = [ //html 
-        `${r.location.addr.city}, ${r.location.addr.street} ${r.location.addr.nr}. ${r.location.res.floor} ${r.location.res.direction}`,
-         `${takeLeft(r.rev.review, 15)} ...<a href="#" onclick="toggleModalReviews('reviewDetails', ${serialize(r)})"  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"> more</a>`,
-         r.rev.createdOn,
-         `<button class="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded" disabled>
+        const row = [ //html 
+          `${r.location.addr.city}, ${r.location.addr.street} ${r.location.addr.nr}. ${r.location.res.floor} ${r.location.res.direction}`,
+          `${takeLeft(r.rev.review, 15)} ...<a href="#" onclick="toggleModalReviews('reviewDetails', ${serialize(r)})"  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"> more</a>`,
+          status,
+          r.rev.createdOn,
+          `<button class="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded" disabled>
          Delete
        </button>`]
 
         revDataTable.row.add(row)
-        .draw(false);
-    });
+          .draw(false);
+      });
 
-})
+  })
