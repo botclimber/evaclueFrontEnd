c9de51504cd5655d4935385178f0e42be3ac8602
Daniel Silva <daniel.silva.prg@gmail.com>
2024-02-20 15:31:40 +0000

    not possible to send message to owner on front end if not logged in

--- src/pages/main/resOwner.js
+++ src/pages/main/resOwner.js
@@ -1,6 +1,6 @@
 const msgToOwner = document.getElementById("msgToOwner");
 
-function toggleModalContactResOwner(modalID){
+function toggleModalContactResOwner(modalID) {
     toggleModalAvaResidences("avaResidenceDetails")
 
     document.getElementById(modalID).classList.toggle("hidden");
@@ -9,31 +9,34 @@ function toggleModalContactResOwner(modalID){
     document.getElementById(modalID + "-backdrop").classList.toggle("flex");
 }
 
-async function sendMessageToOwner(){
-    if(!isEmpty(msgOwnerId.value) && !isEmpty(msgToOwner.value) ) {
-        if(confirm("In order to avoid spam you are only allowed to send one message, be sure you have written all you expect!")){
+async function sendMessageToOwner() {
+    if ((token === undefined || token == "undefined")) dialog.info("Login required to perform this action!");
+    else {
+        if (!isEmpty(msgOwnerId.value) && !isEmpty(msgToOwner.value)) {
+            if (confirm("In order to avoid spam you are only allowed to send one message, be sure you have written all you expect!")) {
 
-            const data = {
-                resOwnerId: parseInt(msgOwnerId.value),
-                message: msgToOwner.value
+                const data = {
+                    resOwnerId: parseInt(msgOwnerId.value),
+                    message: msgToOwner.value
+                }
+
+                const response = await fetch(`${apis.notification}emToOwner`, {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json",
+                        "authorization": `baer ${token}`
+                    },
+                    body: JSON.stringify(data)
+                })
+                const json = await response.json()
+
+                if (response.ok) dialog.success("Message sent! thank you.")//window.location.reload();
+                else {
+                    console.log(json)
+                    dialog.err(json.msg)
+                }
+
             }
-
-            const response = await fetch(`${apis.notification}emToOwner`,{
-                method: "POST",
-                headers: {
-                    "Content-type":"application/json",
-                    "authorization": `baer ${token}`
-                },
-                body: JSON.stringify(data)
-            })
-            const json = await response.json()
-
-            if(response.ok) dialog.success("Message sent! thank you.")//window.location.reload();
-            else{ 
-                console.log(json)
-                dialog.err(json.msg)
-            }
-
-        }
-    }else dialog.info("Please write something...") 
+        } else dialog.info("Please write something...")
+    }
 }
